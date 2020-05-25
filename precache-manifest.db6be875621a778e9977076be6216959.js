fc5f27eb3c093d126b92a7621442f41f8b2d72e0
arohlen <arohlen@kth.se>
2020-05-25 21:32:17 +0200

    Updates

---
+++ precache-manifest.db6be875621a778e9977076be6216959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15ecdc14220b2154784f246bbacd20a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d711435fec6bfced589",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b320b37a370356acda17",
+    "url": "/static/js/2.4f672cb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f672cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d711435fec6bfced589",
+    "url": "/static/js/main.1b58b731.chunk.js"
+  },
+  {
+    "revision": "0520c2775a53512b1ae2",
+    "url": "/static/js/runtime-main.0dcdf6cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
